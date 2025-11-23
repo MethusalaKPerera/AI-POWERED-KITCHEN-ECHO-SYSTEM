1b84c308f931a5c2de1aab526ab9b5df2c803aa8
Mohamed Shahmi <it22083982@my.sliit.lk>
2025-11-23 12:07:22 +0530

    Backend Initialization

--- Frontend/src/Pages/SmartShopping.jsx
+++ Frontend/src/Pages/SmartShopping.jsx
@@ -1,21 +1,4 @@
 import React from "react";
-import { AppRouter } from "./Smart Shopping/AppRouter";
-import { LanguageProvider } from "./Smart Shopping/context/LanguageContext";
-import { CurrencyProvider } from "./Smart Shopping/context/CurrencyContext";
-import { HistoryProvider } from "./Smart Shopping/context/HistoryContext";
+import { App } from "./SmartShoppingApp/App";
 
-function SmartShopping() {
-  return (
-    <LanguageProvider>
-      <CurrencyProvider>
-        <HistoryProvider>
-          <div className="w-full min-h-screen bg-gray-50">
-            <AppRouter />
-          </div>
-        </HistoryProvider>
-      </CurrencyProvider>
-    </LanguageProvider>
-  );
-}
-
-export default SmartShopping;
+export default App;
